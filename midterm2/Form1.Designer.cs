53a148b94e23102318fd17f32b90c5e8dcfabad6
Takahiro FUJIWARA <fujiwat0601@gmail.com>
2023-06-22 15:38:46 +0200

    Add project files.

---
+++ midterm2/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace midterm2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PicSmile = new System.Windows.Forms.PictureBox();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.TxtYourName = new System.Windows.Forms.TextBox();
+            this.LblYourName = new System.Windows.Forms.Label();
+            this.BtnOk = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.PicGoal = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PicSmile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicGoal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PicSmile
+            // 
+            this.PicSmile.Image = ((System.Drawing.Image)(resources.GetObject("PicSmile.Image")));
+            this.PicSmile.Location = new System.Drawing.Point(60, 50);
+            this.PicSmile.Name = "PicSmile";
+            this.PicSmile.Size = new System.Drawing.Size(30, 30);
+            this.PicSmile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PicSmile.TabIndex = 0;
+            this.PicSmile.TabStop = false;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimer.Location = new System.Drawing.Point(207, 240);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(183, 23);
+            this.lblTimer.TabIndex = 1;
+            this.lblTimer.Text = "Score";
+            this.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblGameOver
+            // 
+            this.lblGameOver.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameOver.ForeColor = System.Drawing.Color.Red;
+            this.lblGameOver.Location = new System.Drawing.Point(216, 189);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(175, 29);
+            this.lblGameOver.TabIndex = 3;
+            this.lblGameOver.Text = "Game Over";
+            this.lblGameOver.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblGameOver.Visible = false;
+            // 
+            // TxtYourName
+            // 
+            this.TxtYourName.Location = new System.Drawing.Point(211, 284);
+            this.TxtYourName.Name = "TxtYourName";
+            this.TxtYourName.Size = new System.Drawing.Size(179, 19);
+            this.TxtYourName.TabIndex = 4;
+            // 
+            // LblYourName
+            // 
+            this.LblYourName.AutoSize = true;
+            this.LblYourName.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblYourName.Location = new System.Drawing.Point(209, 267);
+            this.LblYourName.Name = "LblYourName";
+            this.LblYourName.Size = new System.Drawing.Size(99, 15);
+            this.LblYourName.TabIndex = 5;
+            this.LblYourName.Text = "Enter your name:";
+            // 
+            // BtnOk
+            // 
+            this.BtnOk.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnOk.Location = new System.Drawing.Point(211, 309);
+            this.BtnOk.Name = "BtnOk";
+            this.BtnOk.Size = new System.Drawing.Size(75, 23);
+            this.BtnOk.TabIndex = 6;
+            this.BtnOk.Text = "OK";
+            this.BtnOk.UseVisualStyleBackColor = true;
+            this.BtnOk.Click += new System.EventHandler(this.BtnOk_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(354, 268);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 14);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Score";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblStart
+            // 
+            this.lblStart.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStart.Location = new System.Drawing.Point(207, 218);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(183, 23);
+            this.lblStart.TabIndex = 8;
+            this.lblStart.Text = "Press SPACE-BAR to start";
+            this.lblStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PicGoal
+            // 
+            this.PicGoal.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.PicGoal.Location = new System.Drawing.Point(202, 163);
+            this.PicGoal.Name = "PicGoal";
+            this.PicGoal.Size = new System.Drawing.Size(196, 185);
+            this.PicGoal.TabIndex = 9;
+            this.PicGoal.TabStop = false;
+            this.PicGoal.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.BtnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 595);
+            this.Controls.Add(this.lblStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnOk);
+            this.Controls.Add(this.LblYourName);
+            this.Controls.Add(this.TxtYourName);
+            this.Controls.Add(this.lblGameOver);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.PicSmile);
+            this.Controls.Add(this.PicGoal);
+            this.Name = "Form1";
+            this.Text = "Labyrinth";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.PicSmile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicGoal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PicSmile;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblGameOver;
+        private System.Windows.Forms.TextBox TxtYourName;
+        private System.Windows.Forms.Label LblYourName;
+        private System.Windows.Forms.Button BtnOk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.PictureBox PicGoal;
+    }
+}
+
---
+++ midterm2/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace midterm2
+{
+
+    public partial class Form1 : Form
+    {
+        static readonly string fileNameWithoutExtention = "scores";
+        static readonly string SAVE_TXT_FILE_NAME = System.Reflection.Assembly.GetEntryAssembly().Location + @"\..\..\" + fileNameWithoutExtention + ".txt";
+        Point StartPos;
+        Boolean gameoverFlag = true;
+        Stopwatch stopWatch = new Stopwatch();
+        private Point[] labyPoint =
+        {
+            new Point {X=50,Y=50 },
+            new Point {X=50, Y=500},
+            new Point {X=500, Y=500},
+            new Point {X=500, Y=50 },
+            new Point {X=100, Y=50},
+            new Point {X=100, Y=450},
+            new Point {X=450, Y=450},
+            new Point {X=450, Y=100},
+            new Point {X=150, Y=100},
+            new Point {X=150, Y=400},
+            new Point {X=400, Y=400},
+            new Point {X=400, Y=150},
+            new Point {X=200, Y=150},
+            new Point {X=200, Y=350},
+            new Point {X=350, Y=350}
+        };
+        public void DrawLaby(Graphics f)
+        {
+            Pen blackPen = new Pen(Color.Black, 3);
+            //            PicSmile.Visible = false;
+            f.DrawLines(blackPen, labyPoint);
+            stopWatch.Start();
+            PicSmile.Visible = true;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Graphics f = this.CreateGraphics();
+            StartPos.X = PicSmile.Left;
+            StartPos.Y = PicSmile.Top;
+            LblYourName.Visible = false;
+            TxtYourName.Visible = false;
+            lblTimer.Text = "";
+            BtnOk.Visible = false;
+            //            DrawLaby(f);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawLaby(e.Graphics);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int x, y;
+            x = PicSmile.Location.X;
+            y = PicSmile.Location.Y;
+            int pitch = 10;
+
+            if (gameoverFlag==true )
+            {
+               if ( e.KeyCode == Keys.Space)
+               {   // game start
+                    PicSmile.Top = StartPos.Y;
+                    PicSmile.Left = StartPos.X;
+                    timer1.Enabled = true;
+                    gameoverFlag = false;
+                    lblGameOver.Visible = false;
+                    lblStart.Visible = false;
+                    stopWatch.Stop();
+                    stopWatch.Reset();
+                    Stopwatch.StartNew();
+               }
+                return;
+            }
+
+
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    if (x < this.Size.Width - PicSmile.Width * 2 - pitch)
+                    {
+                        x += pitch;
+                    }
+                    PicSmile.Location = new System.Drawing.Point(x, y);
+                    break;
+                case Keys.Left:
+                    if (x > pitch)
+                    {
+                        x -= pitch;
+                    }
+                    PicSmile.Location = new System.Drawing.Point(x, y);
+                    break;
+                case Keys.Up:
+                    if (y > pitch)
+                    {
+                        y -= pitch;
+                    }
+                    PicSmile.Location = new System.Drawing.Point(x, y);
+                    break;
+                case Keys.Down:
+                    if (y < this.Size.Height - PicSmile.Height * 3 - pitch)
+                    {
+                        y += pitch;
+                    }
+                    PicSmile.Location = new System.Drawing.Point(x, y);
+                    break;
+                default:
+                    break;
+            }
+            this.Refresh();
+            for (int i=0; i< labyPoint.Length-1; i++)
+            {
+                int x0 = Math.Min(labyPoint[i].X, labyPoint[i + 1].X);
+                int x1 = Math.Max(labyPoint[i].X, labyPoint[i + 1].X);
+                int y0 = Math.Min(labyPoint[i].Y, labyPoint[i + 1].Y);
+                int y1 = Math.Max(labyPoint[i].Y, labyPoint[i + 1].Y);
+                if ( ((x0 == x1) && (PicSmile.Left < x0) && (x1 < PicSmile.Right  && y0 < PicSmile.Top  && PicSmile.Bottom < y1)) 
+                ||   ((y0 == y1) && (PicSmile.Top  < y0) && (y1 < PicSmile.Bottom && x0 < PicSmile.Left && PicSmile.Right  < x1)) )
+                {   // Touched the line
+                    gameOver();
+                    break;
+                }
+            }
+            if (PicGoal.Left < PicSmile.Left && PicSmile.Right < PicGoal.Right
+            &&  PicGoal.Top  < PicSmile.Top  && PicSmile.Bottom < PicGoal.Bottom)
+            {
+                goal();
+            }
+
+        }
+
+        private void gameOver()
+        {
+            lblGameOver.Text = "Game Over.";
+            gameoverFlag = true;
+            lblGameOver.Visible = true;
+            lblStart.Visible = true; ;
+            stopWatch.Stop();
+            timer1.Enabled = false;
+        }
+        private void goal()
+        {
+            lblGameOver.Text = "GOAL!";
+            gameoverFlag = true;
+            lblGameOver.Visible = true;
+            lblStart.Visible = true;
+            stopWatch.Stop();
+            timer1.Enabled = false;
+            LblYourName.Visible = true;
+            TxtYourName.Visible = true;
+            BtnOk.Enabled = true;
+            BtnOk.Visible = true;
+            TxtYourName.Focus();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10).Trim();
+            lblTimer.Text = elapsedTime;
+        }
+        private void BtnOk_Click(object sender, EventArgs e)
+        {
+            StreamWriter swTxtFile;
+            if (File.Exists(SAVE_TXT_FILE_NAME))
+            { // file exist -- appendmode
+                swTxtFile = new StreamWriter(SAVE_TXT_FILE_NAME, true, Encoding.UTF8);
+            }
+            else
+            { // new
+                swTxtFile = new StreamWriter(SAVE_TXT_FILE_NAME, false, Encoding.UTF8);
+            }
+            swTxtFile.WriteLine(TxtYourName.Text + "," + lblTimer.Text);
+
+            swTxtFile.Close();
+            LblYourName.Visible = false;
+            TxtYourName.Visible = false;
+            BtnOk.Enabled = false;
+            BtnOk.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(SAVE_TXT_FILE_NAME);
+        }
+    }
+}
